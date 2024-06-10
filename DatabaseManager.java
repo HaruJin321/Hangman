66454e52966a482c285adfa618eb7236fe58edc4
HaruJin321 <114570522+HaruJin321@users.noreply.github.com>
2024-06-10 21:21:46 +1200

    New Table DatabaseManager.java

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -9,6 +9,7 @@ public class DatabaseManager {
 
     private static final String DB_URL = "jdbc:derby:hangmandb_Ebd;create=true";
     private static final String CREATE_TABLE_USERS = "CREATE TABLE Users (username VARCHAR(50) PRIMARY KEY, score INT)";
+    private static final String CREATE_TABLE_GUESSED = "CREATE TABLE GuessedLetters (letter CHAR(1) PRIMARY KEY)";
     
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL);
@@ -28,7 +29,28 @@ public class DatabaseManager {
                 e.printStackTrace();
             }
         }
+        
+        
+        try (Connection conn = getConnection(); Statement stmt = conn.createStatement())
+        {
+             
+            stmt.executeUpdate(CREATE_TABLE_GUESSED);
+        } 
+        catch (SQLException e) 
+        {
+            if (e.getSQLState().equals("X0Y32")) {
+            } 
+            else 
+            {
+                e.printStackTrace();
+            }
+        }
+        
+        
+        
     }
+    
+    
     
     
     
