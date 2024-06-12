e3604396c87dbf9e7cdfe24cf2631b1c2d431ad0
HaruJin321 <114570522+HaruJin321@users.noreply.github.com>
2024-06-12 15:50:34 +1200

    Update InputManager.java

--- InputManager.java
+++ InputManager.java
@@ -4,18 +4,21 @@ package Hangman;
 
 public class InputManager {
     
-    private WordManager wordManager = new WordManager();
-    private String word = wordManager.ChosenWord().toLowerCase();
+    private WordManager wordManager = new WordManager();//new wordmanager instance 
+    private String word = wordManager.ChosenWord().toLowerCase(); // get chosen word and convert it to lowercase 
     
+    
+    //validates the users input
     public String validateUserInput(String userInput) {
-        userInput = userInput.toLowerCase();
+        userInput = userInput.toLowerCase(); // Converts the user input to lowervase
         
-        if (userInput.matches("[^a-zA-Z]")) {
-            return "Error! Must guess a letter!";
+        
+        if (userInput.matches("[^a-zA-Z]")) { //Check if the input is not a letter
+            return "Error! Must guess a letter!"; // return message to gui
         } 
-        else if (userInput.isEmpty()) {
+        else if (userInput.isEmpty()) { // check if input empty
             return "Error! Guess a letter!";
-        } else if (userInput.length() > 1) {
+        } else if (userInput.length() > 1) { //Check if the input is more than 1 letter
             return "Error! Must guess a single letter!";
         }
         return null;  // null indicates the input is valid
